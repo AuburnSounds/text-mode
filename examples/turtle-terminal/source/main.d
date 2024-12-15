51a64c80c1b46ea51d6b57a8bcf237c49a25b02c
Guillaume Piolat <contact@auburnsounds.com>
2024-12-15 17:58:07 +0100

    xhars

--- examples/turtle-terminal/source/main.d
+++ examples/turtle-terminal/source/main.d
@@ -55,7 +55,7 @@ class TermExample : TurtleGame
 
             // Change foreground color (0 to 15)
             fg(TM_white);
-            print("≐	≑	≒	≓	≔	≕	≖	≗	≘	≙	≚	≛	≜	≝	≞	≟");
+            println("≐≑≒≓≔≕≖≗≘≙≚≛≜≝≞≟"); 
             bg(TM_blue);
             println("on blue background");
 
