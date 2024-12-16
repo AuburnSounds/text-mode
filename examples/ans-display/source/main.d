6b85bb65ebba13f21408b121f3b7863b8e0d9031
Guillaume Piolat <contact@auburnsounds.com>
2024-12-16 19:43:19 +0100

    Towards displaying .ans

---
+++ examples/ans-display/source/main.d
@@ -0,0 +1,53 @@
+import turtle;
+import textmode;
+
+int main(string[] args)
+{
+    runGame(new TermExample());
+    return 0;
+}
+
+// Note: the "turtle" game engine now has a builtin text-mode console,
+// this is an example of how it could be integrated manually;
+class TermExample : TurtleGame
+{
+    this()
+    {
+
+    }
+
+    override void load()
+    {
+        setBackgroundColor(color("black"));
+        console.size(80, 45);
+    }
+
+    override void mouseMoved(float x, float y, float dx, float dy)
+    {
+    }
+
+    override void update(double dt)
+    {
+        console.update(dt);
+        if (keyboard.isDown("escape")) exitGame();
+    }
+
+    int ntimes;
+    override void draw()
+    {
+        ImageRef!RGBA fb = framebuffer();
+
+        console.palette(TM_Palette.campbell);
+        console.outbuf(fb.pixels, fb.w, fb.h, fb.pitch);
+
+        with (console)
+        {
+            cls();
+            printANS_CP437(import("anst-fenrir.ans"));
+            render();
+        }
+    }
+
+    TM_Console console;
+}
+
