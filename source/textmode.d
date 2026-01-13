283db72fc1f8ec727deced9e5a349711ca2e65ac
Guillaume Piolat <contact@auburnsounds.com>
2026-01-13 13:50:06 +0100

    Tweaks to graillon palette

--- source/textmode.d
+++ source/textmode.d
@@ -3015,10 +3015,10 @@ static immutable uint[16][TM_PALETTE_NUM] PALETTE_DATA =
 
     // Graillon, inspired by Graillon interface
     // https://www.auburnsounds.com/
-    [ 0x1a102400, 0x4a3441ff, 0x7e9e8fff, 0x4f3033ff,
-      0x303091ff, 0x8b326fff, 0x7c93b3ff, 0x917f83ff,
+    [ 0x0b070f00, 0x4a3441ff, 0x7e9e8fff, 0x4f3033ff,
+      0x2e3084ff, 0x8b326fff, 0x7c93b3ff, 0x917f83ff,
       0x3d2d37ff, 0xe96979ff, 0xa9efadff, 0xff9f74ff,
-      0x5555ffff, 0xe652b7ff, 0x97ebebff, 0xe7dcdeff ]
+      0x5f74ffff, 0xe652b7ff, 0x97ebebff, 0xe7dcdeff ]
 ];
 
 alias TM_RangeFlags = int;
